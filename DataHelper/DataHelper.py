a2ddf4a82c42ea9774b9238e93cb65172144cfc9
Quentin Glangetas <qag2000@columbia.edu>
2020-10-19 12:37:04 -0400

    Added DataMaker class and notebook on how to use the class.

--- DataHelper/DataHelper.py
+++ DataHelper/DataHelper.py
@@ -1,6 +1,7 @@
 # Data Helper
 
 from .DataCache import DataCache
+from .DataMaker import DataMaker
 from .DataPreProcessor import DataPreProcessor
 from .DataProcessor import DataProcessor
 from .DataReader import DataReader
@@ -22,3 +23,7 @@ def get_data_cache(clustering_year):
 
 def get_data_processor():
     return DataProcessor()
+
+
+def get_data_maker(data_name=''):
+    return DataMaker(data_name)
